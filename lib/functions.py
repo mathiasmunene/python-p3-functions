64c1299ba88b57c1658f014862991fda3170811c
mathiasmunene <mathiasmunene@gmail.com>
2025-07-01 14:43:03 +0300

    final commit

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,25 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
+
+greet_programmer()
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
+
+greet("Baba")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
+
+greet_with_default()
 
 def add(num1, num2):
-    pass
+    return num1 + num2
+
+add_result = add(1, 2)
+print(add_result)
 
 def halve(number):
-    pass
+    return number / 2
